e083cb911d443695a728565cb48a4ffd5f7395c1
fed12345 <f.magri12@gmail.com>
2023-04-25 08:21:21 +0000

    GPU modification

--- network_training/train.py
+++ network_training/train.py
@@ -4,30 +4,29 @@ import cv2
 import csv
 import os
 import copy
+import tensorflow as tf
 
-sys.path.append("/Users/Federico/Desktop/Thesis/code/visionnet/network_creation")
-sys.path.append("/Users/Federico/Desktop/Thesis/code/MAVLAB_GATES/data/AIRR")
+sys.path.append("visionnet/network/visionnet/network_creation/")
 
 from visionnet import createModel
 from gendataset import Dataset
-import dataset
 
 def visualizePerdiction(image, prediction, actual):
-    image = cv2.cvtColor((image.numpy().astype('float')*255).astype('uint8'), cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor((image.numpy().astype('float')*255).astype('uint8'), cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     for i in range(0, len(prediction), 2):
         cv2.circle(image, (int(prediction[i]), int(prediction[i+1])), 10, (0, 0, 255), -1)
         cv2.circle(image, (int(actual[i]), int(actual[i+1])), 10, (0, 255, 0), -1)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+    return image
 
 if __name__ == "__main__":
    
     # Define the image directory and CSV file
-    imageDirAustin1 = '/Users/Federico/Desktop/Thesis/code/MAVLAB_GATES/data/AIRR/Austin1/'
-    csvFileAustin1 = '/Users/Federico/Desktop/Thesis/code/MAVLAB_GATES/data/AIRR/Austin1/corners.csv'
+    imageDirAustin1 = '/data/federico/visionnet/training_data/Austin1'
+    csvFileAustin1 = '/data/federico/visionnet/training_data/Austin1/corners.csv'
 
-    imageDirAustin2 = '/Users/Federico/Desktop/Thesis/code/MAVLAB_GATES/data/AIRR/Austin2/'
-    csvFileAustin2 = '/Users/Federico/Desktop/Thesis/code/MAVLAB_GATES/data/AIRR/Austin2/corners.csv'
+    imageDirAustin2 = '/data/federico/visionnet/training_data/Austin2'
+    csvFileAustin2 = '/data/federico/visionnet/training_data/Austin2/corners.csv'
 
     # Define the input and output shapes of the model
     input_shape = (600, 360, 3)
@@ -46,21 +45,34 @@ if __name__ == "__main__":
     datasetVal = valAustin1.concatenate(valAustin2)
     print('Datasets Ready')
 
+    strategy = tf.distribute.OneDeviceStrategy(device = '/GPU:2')
     # Create the model
-    model = createModel()
-    model.summary()
-
-    # Train the model
-    model.fit(datasetTrain, epochs=15, validation_data=datasetVal, verbose = 1)
+   
+    with strategy.scope():
+        model = createModel()
+        model.summary()
+        #Train the model
+        model.fit(datasetTrain, epochs=50, validation_data=datasetVal, verbose = 1)
 
     # Evaluate the model on the validation set
     valLoss, valAcc, _ = model.evaluate(datasetVal)
     print('Validation loss:', valLoss)
     print('Validation accuracy:', valAcc)
-
     #Visualize the Predictions
     predictions = model.predict(datasetVal)
     imagesVal, labelsVal = next(iter(datasetVal))
     for i in range(len(imagesVal)):
-        visualizePerdiction(imagesVal[i], predictions[i], labelsVal[i])
+        cv2.imwrite('visionnet/network/visionnet/evalutation/'+ str(i) + '.png',visualizePerdiction(imagesVal[i], predictions[i], labelsVal[i]))
+
+    print('Saving Model')
+    tf.saved_model.save(model, 'visionnet/network/visionnet/evalutation/visionnet')
+
+    # # Convert the model
+    # converter = tf.lite.TFLiteConverter.from_saved_model('visionnet/network/visionnet/evalutation/visionnet') # path to the SavedModel directory
+    # tflite_model = converter.convert()
+    
+    # # Save the model.
+    # with open('visionnet.tflite', 'wb') as f:
+    #     f.write(tflite_model)   
+
 
